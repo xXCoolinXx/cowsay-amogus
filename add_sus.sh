09486080529e530d68c23b12a3260ad1522e3f74
Coolin <collin.t.francel@gmail.com>
2022-07-02 15:43:01 -0500

    final commit

--- add_sus.sh
+++ add_sus.sh
@@ -3,4 +3,4 @@
 echo "Adding amogus to cowsay options"
 mv sus.cow /usr/share/cows/ 
 
-echo "Use the sussy baka by running '\$ cowsay -f sus <text here>'
+echo "Use the sussy baka by running '\$ cowsay -f sus <text here>'"
